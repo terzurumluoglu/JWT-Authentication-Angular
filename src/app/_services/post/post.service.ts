842b817d95e0c1b6fee5da68f92007645acdc799
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-09-29 16:43:54 +0300

    Update PostService

--- src/app/_services/post/post.service.ts
+++ src/app/_services/post/post.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Post } from "../../_models";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getAllPosts(){
+    const url : string = `${environment.apiUrl}posts`;
+    let options = {
+      headers : new HttpHeaders().set('Content-Type', 'application/json')
+    }
+    return this.http.get<Post[]>(url,options).toPromise();
+  }
 }
