95cce3f251fa4524686f55ccf6e82fc54f3c08c1
terzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-09-29 16:44:35 +0300

    Update LoginComponent

--- src/app/_components/login/login.component.ts
+++ src/app/_components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/_models';
+import { AuthenticationService } from 'src/app/_services';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm : FormGroup;
+  returnUrl : string;
+  constructor(
+    private fb : FormBuilder,
+    private route: ActivatedRoute,
+    private router : Router,
+    private _auth : AuthenticationService
+    ) {}
 
   ngOnInit(): void {
+    this.createForm();
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  get f(){
+    return this.loginForm.value;
+  }
+
+  createForm(){
+    this.loginForm = this.fb.group({
+      email : ['toprak@toprak.com',[Validators.required]],
+      password : ['123456',[Validators.required]]
+    })
+  }
+
+  async submit(){
+    const {email,password} = this.f;
+    const user : User = await this._auth.logIn(email,password).toPromise();
+    this.router.navigate(['/home']);
+  }
 }
